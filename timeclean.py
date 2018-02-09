697585e0d2e713bfd5a1f615795e2d07f708e866
willhaslam <williamhaslam88@gmail.com>
2018-02-09 15:30:43 +0000

    fixed formatting for pep8

--- timeclean.py
+++ timeclean.py
@@ -4,38 +4,41 @@ import numpy as np
 
 def data_cleaner(dataframe, Datetime_column, time_interval, outcome_column, time_steps_to_evaluate=1, time_steps_look_ahead=0, interprolation_direction='forward'):
 
+    if interprolation_direction == 'forward':
+        interprolation_direction = 'ffill'
+    elif interprolation_direction == 'backward':
+        interprolation_direction = 'bfill'
 
+    # sort by DateTime
+    df_sorted = dataframe.sort_values(Datetime_column)
 
-  if interprolation_direction == 'forward':
-    interprolation_direction = 'ffill'
-  elif interprolation_direction == 'backward':
-    interprolation_direction = 'bfill'
+    # Deletes duplicates, keeps last value, and sets DateTime as index
+    indexed_df = df_sorted.drop_duplicates(
+        Datetime_column, keep='last').set_index(Datetime_column)
 
-  #sort by DateTime
-  df_sorted = dataframe.sort_values(Datetime_column)
+    # Creates consistent intervals according to input
+    regularized_intervals = indexed_df.asfreq(
+        freq=time_interval, method='ffill')
 
-  #Deletes duplicates, keeps last value, and sets DateTime as index
-  indexed_df = df_sorted.drop_duplicates(Datetime_column, keep='last').set_index(Datetime_column)
+    # separates data that will be the Y from X
+    Y_data = regularized_intervals[outcome_column].to_frame(
+        name=outcome_column)
+    X_data = regularized_intervals.drop([outcome_column], axis=1)
 
-  #Creates consistent intervals according to input
-  regularized_intervals = indexed_df.asfreq(freq=time_interval, method='ffill')
+    X_reg_intervals, Y_reg_intervals = [], []
 
-  #separates data that will be the Y from X
-  Y_data = regularized_intervals[outcome_column].to_frame(name=outcome_column)
-  X_data = regularized_intervals.drop([outcome_column], axis =1)
+    # time_steps_to_evaluate will be the number of time steps in each instance of the X data, time_steps_look_ahead will be the number of time steps the model will look ahead
+    for i in range(len(X_data) - time_steps_to_evaluate - time_steps_look_ahead):
+        x = X_data.iloc[i:(i+time_steps_to_evaluate)].values
+        X_reg_intervals.append(
+            X_data.iloc[i:(i+time_steps_to_evaluate)].values)
+        Y_reg_intervals.append(
+            Y_data.iloc[i + time_steps_to_evaluate + time_steps_look_ahead - 1].values)
 
-  X_reg_intervals, Y_reg_intervals = [], []
+    # reshapes and converts to np arrays
 
-  #time_steps_to_evaluate will be the number of time steps in each instance of the X data, time_steps_look_ahead will be the number of time steps the model will look ahead
-  for i in range(len(X_data) - time_steps_to_evaluate - time_steps_look_ahead):
-    x = X_data.iloc[i:(i+time_steps_to_evaluate)].values
-    X_reg_intervals.append(X_data.iloc[i:(i+time_steps_to_evaluate)].values)
-    Y_reg_intervals.append(Y_data.iloc[i + time_steps_to_evaluate + time_steps_look_ahead -1].values)
+    X_output_data = np.array(X_reg_intervals).reshape(
+        len(X_reg_intervals), X_data.shape[1], time_steps_to_evaluate)
+    Y_output_data = np.array(Y_reg_intervals).reshape(len(Y_reg_intervals), 1)
 
-  #reshapes and converts to np arrays
-
-  X_output_data = np.array(X_reg_intervals).reshape(len(X_reg_intervals), X_data.shape[1], time_steps_to_evaluate)
-  Y_output_data = np.array(Y_reg_intervals).reshape(len(Y_reg_intervals), 1)
-
-  return X_output_data, Y_output_data
-
+    return X_output_data, Y_output_data
